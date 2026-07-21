c4e0bd012c7ad56245f58a8c5d7d1bd9ec11fe2f
FNA <fna@example.com>
2026-07-21 15:04:27 +0200

    changed to submodule

--- 20_ROS/src/eta_rmf/docker/join.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-CONTAINER_NAME="eta_fleet_container"
-echo "Using Container Name: $CONTAINER_NAME"
-docker exec -it "$CONTAINER_NAME" bash
--- 20_ROS/src/tc_ros2_itf/docker/join.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-CONTAINER_NAME="eta_tc_ros2_itf_container"
-echo "Using Container Name: $CONTAINER_NAME"
-docker exec -it "$CONTAINER_NAME" bash
